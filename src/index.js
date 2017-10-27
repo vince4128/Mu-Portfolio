b17fed37c9db48231a461c5ff19f42913a17e5d2
vince4128 <vrlt.vincent@gmail.com>
2017-10-27 16:53:32 +0200

    style file added

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+
+require("../style/style.scss");
