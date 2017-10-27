0d518d1272f9eb9cb093ab340886116ada4160ac
vince4128 <vrlt.vincent@gmail.com>
2017-10-27 17:32:43 +0200

    root component added

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <section>
+                <p>Root Component</p>
+            </section>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 require("../style/style.scss");
+
+import App from './components/app';
+
+ReactDOM.render(
+    <App />, document.querySelector('.container')
+  );
+  
