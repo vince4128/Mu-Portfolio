5622a5df61c56c41ef113eb0baa05bc4fc0daaa5
vince4128 <vrlt.vincent@gmail.com>
2017-11-01 21:14:43 +0100

    css class conditionnally added

--- src/components/ProjectScreen.js
+++ src/components/ProjectScreen.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const ProjectScreen = (props) => {
 
+    function isWhat(index){
+        if(index === props.projet.id){
+            return "c-projectScreen--active"
+        }else if(index === (props.projet.id - 1)){
+            return "c-projectScreen--prev"
+        }else if(index === (props.projet.id + 1)){
+            return "c-projetScreen--next"
+        }
+    }
+
     return(
-        <li onClick={()=>props.onProjectSelect(props.projet.id)}>
+        <li className={"c-projectScreen " + isWhat(props.projet.id)} onClick={()=>props.onProjectSelect(props.projet.id)}>
             <p>projet : {props.projet.projet}</p>
         </li>
     )
