cbf1e4773170abe4f999fd92a58bb26052d7e788
vince4128 <vrlt.vincent@gmail.com>
2017-11-05 21:34:02 +0100

    app rewrite as a funcionnal component

--- src/components/app.js
+++ src/components/app.js
@@ -1,72 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import ProjectList from '../containers/ProjectList';
+import ProjectDetail from '../containers/ProjectDetail';
 
 import ProjectViewer from './ProjectViewer';
 import ProjectPagination from './ProjectPagination';
 import PrevNext from './PrevNext';
 
-const fakeProject = [
-    {
-        projet: "projet1",
-        desc: "Description du projet",
-        img: "img1.jpg",
-        id: 0
-    },
-    {
-        projet: "projet2",
-        desc: "Description du projet",
-        img: "img2.jpg",
-        id: 1
-    },
-    {
-        projet: "projet3",
-        desc: "Description du projet",
-        img: "img3.jpg",
-        id: 2
-    },
-    {
-        projet: "projet4",
-        desc: "Description du projet",
-        img: "img4.jpg",
-        id: 3
-    }
-];
 
-class App extends Component {
+const App = (props) => {
 
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-            selectedProject: 0,
-            totalProject: fakeProject.length
-        };
-    }
+    return (
+        <section>
+            <ProjectList />
+            <ProjectDetail />
+        </section>
+    );
 
-    render() {
-        return (
-            <section>
-                <PrevNext
-                    onProjectSelect={selectedProject => this.setState({selectedProject})}//update app state
-                    projectSelected={this.state.selectedProject}
-                    totalProject={this.state.totalProject}
-                />
-                <ProjectViewer
-                    projets={fakeProject}
-                    onProjectSelect={selectedProject => this.setState({selectedProject})}//update app state
-                    projectSelected={this.state.selectedProject}
-                    totalProject={this.state.totalProject} 
-                />
-                <p>selectedProject : {this.state.selectedProject}</p>
-                <ul className="c-projectPagination">
-                    <ProjectPagination 
-                        projectSelected={this.state.selectedProject}
-                        totalProject={this.state.totalProject}
-                        onProjectSelect={selectedProject => this.setState({selectedProject})}//update app state
-                    />
-                </ul>
-            </section>
-        );
-    }
 }
 
 export default App;
