d837421174ccafdbd8779927ff47964d612cde43
vince4128 <vrlt.vincent@gmail.com>
2017-11-06 17:38:39 +0100

    cleaned

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,6 @@ import React from 'react';
 import ProjectList from '../containers/ProjectList';
 import ProjectDetail from '../containers/ProjectDetail';
 
-import ProjectViewer from './ProjectViewer';
-import ProjectPagination from './ProjectPagination';
-import PrevNext from './PrevNext';
-
-
 const App = (props) => {
 
     return (
