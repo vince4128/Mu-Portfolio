c99fd83bcd50537017bf61284acada1334491123
vince4128 <vrlt.vincent@gmail.com>
2017-11-06 17:38:24 +0100

    Router added (not working yet)

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 require("../style/style.scss");
 
 import App from './components/app';
+import ProjectList from './containers/ProjectList';
+import ProjectDetail from './containers/ProjectDetail';
+
 import reducers from './reducers';
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
-    <App />
-   </Provider>
+    <BrowserRouter>
+      <section>
+        <Switch>
+          <Route path="/" component={App}/>
+          <Route path="/projets" component={ProjectList}/>
+          <Route path="/projets-detail/:id" component={ProjectDetail}/>
+        </Switch>
+      </section>
+    </BrowserRouter>
+  </Provider>
   , document.querySelector('.container')
   );
   
