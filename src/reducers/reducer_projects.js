f647b5ae3587c00817110edd901c3e0b14cd51e6
vince4128 <vrlt.vincent@gmail.com>
2017-11-11 23:21:55 +0100

    reducer_project added

---
+++ src/reducers/reducer_projects.js
@@ -0,0 +1,19 @@
+import _ from 'lodash';
+import {FETCH_PROJECTS, FETCH_PROJECT} from '../actions';
+
+export default function(state = {}, action) {
+    switch(action.type) {
+        case FETCH_PROJECTS:
+            console.log("FETCH_PROJECTS lancé depuis reducer_project !");
+            console.log(action.payload);
+            return _.mapKeys(action.payload, "id");
+            //return {1:{"id":1},2:{"id":2}};
+
+        case FETCH_PROJECT:
+        return { ...state, [action.payload.id]: action.payload };
+
+        default:
+            return state;
+    }
+
+}
