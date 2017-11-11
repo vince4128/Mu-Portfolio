52f6abd1c6084a15df971a696a54ce8c538a10f7
vince4128 <vrlt.vincent@gmail.com>
2017-11-11 23:19:19 +0100

    action rewrited

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,89 @@
-export function selectProject(project){
+import axios from 'axios';
+import lodash from 'lodash';
+
+export const FETCH_PROJECTS = "fetch_projects";
+export const FETCH_PROJECT = "fetch_project";
+
+const PROJECT = [
+    {
+        id:0,
+        title: 'Le projet 1',
+        description: 'Blabla1',
+        images: [
+            {
+                src:"img1.jpg",
+                index:0
+            },
+            {
+                src:"img2.jpg",
+                index:1
+            }
+        ]
+    },
+    {
+        id:1, 
+        title: 'Le projet 2',
+        description: 'Blabla2',
+        images: [
+            {
+                src:"img3.jpg",
+                index:0
+            },
+            {
+                src:"img4.jpg",
+                index:1
+            }
+        ]
+    },
+    {
+        id:2, 
+        title: 'Le projet 3',
+        description: 'Blabla3',
+        images: [
+            {
+                src:"img1.jpg",
+                index:0
+            },
+            {
+                src:"img2.jpg",
+                index:1
+            }
+        ]
+    },
+    {
+        id:3, 
+        title: 'Le projet 4',
+        description: 'Blabla4',
+        images: [
+            {
+                src:"img3.jpg",
+                index:0
+            },
+            {
+                src:"img4.jpg",
+                index:1
+            }
+        ]
+    }
+]
+
+export function fetchProjects(){
+    console.log("action fetchProjects lancée !");
     return {
-        type: 'PROJECT_SELECTED',
-        payload: project
+        type: FETCH_PROJECTS,
+        payload: PROJECT
     };
+}
+
+export function fetchProject(id){
+    
+    console.log("action fetchProject lancée !");
+    const projects = _.mapKeys(PROJECT, "id");
+    const project = _.get(projects, id);
+    console.log("project : " + JSON.stringify(project));
+
+    return {
+        type: FETCH_PROJECT,
+        payload: project
+    }
 }
