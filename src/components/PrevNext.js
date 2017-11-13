776a96d28dd78cceb30509d3f73a9e1d052256e4
vince4128 <vrlt.vincent@gmail.com>
2017-11-13 21:58:47 +0100

    ProjectDetail renamed to PrevNext

---
+++ src/components/PrevNext.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const PrevNext = (props) => {
+
+    function prev(){
+        if(props.currentImage > 0){
+            return props.currentImage - 1;
+        }else{
+            return props.totalImages-1;
+        }
+    };
+
+    function next(){
+        if(props.currentImage < props.totalImages-1){
+            return props.currentImage + 1;
+        }else{
+            return 0;
+        }
+    };
+
+    return(
+        <div className="c-prevnext">
+            <div
+                className="c-prevnext__prev"
+                onClick={()=>props.selectImage(prev())}>prev
+            >
+            </div>
+            <div
+                className="c-prevnext__next"
+                onClick={()=>props.selectImage(next())}>next
+            >
+            </div>
+        </div>
+    )
+
+}
+
+export default PrevNext;
