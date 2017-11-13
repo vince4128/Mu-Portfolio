aafffeb269041e8756d73d2c2909b0e3421bfc26
vince4128 <vrlt.vincent@gmail.com>
2017-11-13 15:13:33 +0100

    change classes to make project slide

--- src/components/ProjectAllViewer.js
+++ src/components/ProjectAllViewer.js
@@ -1,46 +1,73 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ProjectAllViewer extends Component {
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            status:''
+        }
+    }
+
     componentWillReceiveProps(nextProps, oldProps){
-        /*
-        //image actuellement affichée
-        if(nextProps.image.index === nextProps.currentImage){
+
+        //projet a afficher
+        if(nextProps.project.id == nextProps.currentProject){
+            //alert("nouvel slide !");
             this.setState({
                 status: 'c-projectScreen--active slideInRight'
             });
-        }//image précédente
-        else if(nextProps.image.index === this.props.currentImage){
+        }//projet a faire défiler
+        else if(nextProps.project.id == this.props.currentProject){
             this.setState({
                 status: 'c-projectScreen--prev slideOutLeft'
             });
-        }
+        }//projet a masquer
         else{
             this.setState({
-                status:''
+                status: ''
             })
         }
-        */
+
     }
 
     componentWillMount() {
-    
-        //afficher la première image
-        /*if(this.props.image.index === this.props.currentImage){
+
+        //afficher le premier projet
+        if(this.props.project.id == this.props.currentProject){
             this.setState({
                 status: 'c-projectScreen--active'
             });
-        }*/
-
+        }
     }
 
     render(){
         return(
-            <li>
-                <img src={"/img/"+this.props.image}/>
-                {/*<img src="/img/img4.jpg"/>*/}
+            <div
+                className={"c-projectScreen animated " + this.state.status}
+            >                
                 ProjectAllViewer
-            </li>
+                {this.props.project.title}
+                {JSON.stringify(this.props)}
+                <img src={"/img/"+this.props.project.images[0].src}/>
+                <Link to={`/projets/${this.props.project.id}/detail`}>
+                    detail
+                </Link>
+                <Link to={`/projets/0`}>
+                    0
+                </Link>
+                <Link to={`/projets/1`}>
+                    1
+                </Link>
+                <Link to={`/projets/2`}>
+                    2
+                </Link>
+                <Link to={`/projets/3`}>
+                    3
+                </Link>
+            </div>
         )
     }
 
