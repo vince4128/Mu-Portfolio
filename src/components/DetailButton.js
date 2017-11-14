e17af244d266be308722919ba37e05001166fd79
vince4128 <vrlt.vincent@gmail.com>
2017-11-14 22:24:08 +0100

    DetailButton added

---
+++ src/components/DetailButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DetailButton = (props) => {
+
+    return(
+        <div className="c-button-detail">
+            <h1>DETAIL BUTTON</h1>
+            <div>
+                <button>
+                    Détail
+                </button>
+            </div>
+        </div>
+    )
+
+}
+
+export default DetailButton;
