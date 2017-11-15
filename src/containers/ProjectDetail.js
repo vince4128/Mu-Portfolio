96fa06fd902ba7694537276b908cbe4a6859effd
vince4128 <vrlt.vincent@gmail.com>
2017-11-15 22:59:44 +0100

    renderList added

--- src/containers/ProjectDetail.js
+++ src/containers/ProjectDetail.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { fetchProject } from '../actions/index';
 
 import ProjectDetailViewer from '../components/ProjectDetailViewer';
+import ProjectDetailScreen from '../components/ProjectDetailScreen';
+import PrevNext from '../components/PrevNext';
 
 class ProjectDetail extends Component {
 
@@ -11,27 +13,52 @@ class ProjectDetail extends Component {
         super(props);
 
         this.state = {
-            pid:0
+            currentProject:0,
+            currentImage:0,
+            totalImages:4
         }
+
+        this.selectImageurl = this.selectImageurl.bind(this);
     }
 
-    selectImage(imageId){
+    selectImageurl(imageId){
         this.props.history.push(`/projets/${this.props.project.id}/${imageId}`);
     }
 
     componentDidMount(){
         const { id } = this.props.match.params;
-        this.props.fetchProject(id);
         const { pid } = this.props.match.params;
-        this.setState({ pid });
-        //alert(this.state.pid);
+        this.props.fetchProject(id);
+        this.setState({ currentProject:id });
+        this.setState({ currentImage:pid });
     }
 
     componentWillReceiveProps(nextProps, oldProps){
-        //const { id } = this.props.match.params;
-        //const { pid } = nextProps.match.params;
-        //this.setState({pid});
-        //alert(this.state.pid);
+        const { id } = nextProps.match.params;
+        const { pid } = nextProps.match.params;
+        this.setState({ currentProject:id });
+        this.setState({ currentImage:pid });
+    }
+
+    renderList(){
+        return _.map(this.props.project.images, image => {
+            return (
+                <li key={image.index}>
+                <ProjectDetailScreen 
+                    currentImage={parseInt(this.state.currentImage)}
+                    totalImages={this.state.totalImages}
+                    //selectImage={this.selectImage}
+                    image={image}
+                />
+                {/*<PrevNext
+                    select={this.selectProject}
+                    current={parseInt(this.state.currentProject)}
+                    total={this.state.totalProject}
+                    obj={this.props.projects}
+                />*/}
+                </li>
+            );
+        })
     }
 
     render() {
@@ -49,8 +76,25 @@ class ProjectDetail extends Component {
                     <div>Titre : {this.props.project.title}</div>
                     <div>Description : {this.props.project.description}</div>
                 </div>
-                <ProjectDetailViewer 
+                {/*<ProjectDetailViewer 
                     images={this.props.project.images}
+                    current={parseInt}
+                    select={this.selectImageurl}
+                />*/}
+                {/*<ProjectDetailScreen 
+                    currentImage={parseInt(this.state.currentImage)}
+                    totalImages={this.state.totalImages}
+                    //selectImage={this.selectImage}
+                    image={this.props.project.images[this.state.currentImage]}
+                />*/}
+                <ul className="no-overflow">
+                    {this.renderList()}
+                </ul>
+                <PrevNext
+                    select={this.selectImageurl}
+                    current={parseInt(this.state.currentImage)}
+                    total={this.state.totalImages}
+                    obj={this.props.project.images}
                 />
             </section>
         );
