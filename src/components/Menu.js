b55f86f18b92ff19ef25cea3ce8b735b8e0f780d
vince4128 <vrlt.vincent@gmail.com>
2017-11-23 12:27:11 +0100

    link for projets in menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Menu = (props) => {
 
     return(
         <ul className="c-menu">
-            <li>| Index</li>
+            <li>
+                <Link to={`/projets`}>
+                | Index
+                </Link>
+            </li>
             <li>| À propos</li>
         </ul>
     )
