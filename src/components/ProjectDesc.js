1d62dd64ffdc00ae4409bdc672e2a69fdd119a35
vince4128 <vrlt.vincent@gmail.com>
2017-11-26 23:47:36 +0100

    description is collapsible

--- src/components/ProjectDesc.js
+++ src/components/ProjectDesc.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const ProjectDesc = (props) => {
+class ProjectDesc extends Component {
 
-    function close(){
-        alert('yo');
+    constructor(props) {
+        super(props);
+        this.state = {
+            open:true
+        }
+
+        this.isOpened = this.isOpened.bind(this);
+        this.toggleDesc = this.toggleDesc;
     }
 
-    return(
-        <div className="c-project-description c-project-description--open animated fadeIn">
-            <button onClick={()=>close()}>fermer</button>
-            <h1>{props.project.title}</h1>
-            <p>{props.project.description}</p>
-            <hr/>
-            <p>Les liens</p>
-        </div>
-    )
+    toggleDesc(){
+        this.setState({open : !this.state.open})
+    }
+
+    isOpened(){
+        if(this.state.open){
+            return"c-project-description--open";
+        }else{
+            return"c-project-description--close";
+        }
+    }
+
+    render() {
+        return(
+            <div className={"c-project-description " + this.isOpened()}>
+    
+                <section className="c-project-description__toggle animated fadeInUp">
+                    <span>
+                        <button onClick={()=>this.toggleDesc()}>Afficher</button>
+                    </span>
+                </section>
+    
+                <section className="c-project-description__desc animated fadeInUp">
+                    <span>
+                        <button onClick={()=>this.toggleDesc()}>Masquer</button>
+                        <h1>{this.props.project.title}</h1>
+                        <h1>{this.props.project.category} - {this.props.project.year}</h1>
+                        <p>{this.props.project.description}</p>
+                        <hr/>
+                        <p>Les liens</p>
+                    </span>
+                </section>
+    
+            </div>
+        )
+    }
+    
 
 }
 
