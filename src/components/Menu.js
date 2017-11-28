e0de73065215af405cf750bdd876ce1c57ed756a
vince4128 <vrlt.vincent@gmail.com>
2017-11-28 22:37:26 +0100

    Link replaced by NavLink

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Menu = (props) => {
 
-    return(
+    return (
         <ul className="c-menu">
             <li>
-                {/*<a href="/">
-                <h3>| <span>Index</span></h3>
-    </a>*/}
-    <Link to="/">
-                <h3>| <span>Index</span></h3>
-                </Link>
+                <NavLink to="/">
+                    <h3>| <span>Index</span></h3>
+                </NavLink>
             </li>
             <li>
                 <h3>| <span>À propos</span></h3>
