bbc08c7a7a51dcd797b9657c433fd93a95c8fbd3
vince4128 <vrlt.vincent@gmail.com>
2017-12-12 18:04:08 +0100

    handle undefined

--- src/components/ProjectName.js
+++ src/components/ProjectName.js
@@ -2,12 +2,23 @@ import React from 'react';
 
 const ProjectName = (props) => {
 
-    return (
-        <div className={"c-projectName " + props.anim}>
-            <h1><strong>{props.project.title}</strong></h1>
-            <h2>{props.project.category} - {props.project.year}</h2>
-        </div>
-    )
+    if(props.project === undefined){
+        return (
+            
+            <div className={"c-projectName"}>
+            </div>
+        )
+      } else {
+        return (
+            
+            <div className={"c-projectName"}>
+                <h1><strong>{props.project.title}</strong></h1>
+                <h2>{props.project.category} - {props.project.year}</h2>
+            </div>
+        )
+      }
+
+   
 
 }
 
