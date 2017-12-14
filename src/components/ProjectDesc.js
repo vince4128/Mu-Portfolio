3844c16157aadfd0cd3900751f8363748eb217c6
vince4128 <vrlt.vincent@gmail.com>
2017-12-14 22:00:12 +0100

    class add

--- src/components/ProjectDesc.js
+++ src/components/ProjectDesc.js
@@ -54,9 +54,9 @@ class ProjectDesc extends Component {
                 <section className="c-project-description__desc animated fadeInUp">
                     <span>
                         <i className="c-project-description__btn--close fa fa-2x fa-times" aria-hidden="true" onClick={()=>this.toggleDesc()}></i>
-                        <h1>{this.props.project.title}</h1>
-                        <h1>{this.props.project.category} - {this.props.project.year}</h1>
-                        <p>{this.props.project.description}</p>
+                        <h1 class="c-project-description--title">{this.props.project.title}</h1>
+                        <p class="c-project-description--category">{this.props.project.category} - {this.props.project.year}</p>
+                        <p class="c-project-description--text">{this.props.project.description}</p>
                         <hr/>
                         <p>Les liens</p>
                         <p>{this.props.project.description}</p>
