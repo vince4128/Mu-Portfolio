981e7adc16e04f0e51a9cad0e584bdeff1e64d50
vince4128 <vrlt.vincent@gmail.com>
2017-12-19 00:38:27 +0100

    conditionnal rendering

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,23 +4,40 @@ import Apropos from './Apropos';
 
 const Menu = (props) => {
 
-    return (
-        <ul className="c-menu">
-            <li className="c-menu--left">
-                <h3>| <Apropos txt={"À PROPOS"}/></h3>
-            </li>
-            <li className="c-menu--right">
-                <NavLink to="/list">
-                    <h3>| <span>X</span></h3>
-                </NavLink>
-            </li>
-            <li className="c-menu--right">
-                <NavLink to="/list">
-                    <h3>| <span>INDEX</span></h3>
-                </NavLink>
-            </li>
-        </ul>
-    )
+    if(props.parent){
+        return (
+            <ul className="c-menu">
+                <li className="c-menu--left">
+                    <h3>| <Apropos txt={"À PROPOS"}/></h3>
+                </li>
+                <li className="c-menu--right">
+                    <NavLink to={`${props.parent}`}>
+                        <h3>| <i className="c-project-description__btn--close fa fa-2x fa-times" aria-hidden="true"></i></h3>
+                    </NavLink>
+                </li>
+                <li className="c-menu--right">
+                    <NavLink to="/list">
+                        <h3>| <span>INDEX</span></h3>
+                    </NavLink>
+                </li>
+            </ul>
+        )
+    } else{
+        return (
+            <ul className="c-menu">
+                <li className="c-menu--left">
+                    <h3>| <Apropos txt={"À PROPOS"}/></h3>
+                </li>
+                <li className="c-menu--right">
+                    <NavLink to="/list">
+                        <h3>| <span>INDEX</span></h3>
+                    </NavLink>
+                </li>
+            </ul>
+        )
+    }
+
+    
 
 }
 
